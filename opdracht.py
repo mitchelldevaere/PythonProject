18c46edf7af7f3517a59e489b36f49d1a802cd17
mitchelldevaere <michiel.devaere101@gmail.com>
2023-12-23 17:11:36 +0100

    toevoegen van een main functie waarin de data wordt opgehaald door gebruik te maken van de commandline klasse

--- opdracht.py
+++ opdracht.py
@@ -65,3 +65,54 @@ class CommandLineApp:
         query = 'SELECT * FROM orders'
         return self.db_connector.fetch_all(query)
 
+if __name__ == "__main__":
+    db_file = 'database.db'
+    app = CommandLineApp(db_file)
+
+    while True:
+        print("\n1. Add User\n2. Place Order\n3. Get User Info\n4. List All Orders\n5. List all users\n6. Exit")
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            name = input("Enter user name: ")
+            age = input("Enter user age: ")
+            app.add_user(name, age)
+            print("User added successfully!")
+
+        elif choice == '2':
+            user_id = input("Enter user ID: ")
+            product = input("Enter product: ")
+            amount = input("Enter amount: ")
+            app.place_order(user_id, product, amount)
+            print("Order placed successfully!")
+
+        elif choice == '3':
+            user_id = input("Enter user ID: ")
+            user_info = app.get_user_info(user_id)
+            if user_info:
+                print("User Info:")
+                print(user_info)
+            else:
+                print("User not found.")
+
+        elif choice == '4':
+            all_orders = app.get_all_orders()
+            if all_orders:
+                print("All Orders:")
+                for order in all_orders:
+                    print(order)
+            else:
+                print("No orders found.")
+
+        elif choice == '5':
+            all_users = app.get_all_users()
+            if all_users:
+                print("All users:")
+                for user in all_users:
+                    print(user)
+
+        elif choice == '6':
+            break
+
+        else:
+            print("Invalid choice. Please enter a valid option.")
