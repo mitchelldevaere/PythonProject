ef2d8701ab0ac1df97683a95c744ddf593d5716f
mitchelldevaere <michiel.devaere101@gmail.com>
2023-12-23 17:21:46 +0100

    meerdere aanpassingen in de klasse databaseconnector. een fout weggehaald die wel nodig is zodat er parameters kunnen worden meegegeven. verwijderen van code met zelfde functionaliteit door params toe te voegen

--- opdracht.py
+++ opdracht.py
@@ -15,21 +15,18 @@ class DatabaseConnector:
     def connect(self):
         self.conn = sqlite3.connect(self.db_file)
 
-    def execute_query(self, query):
+    def execute_query(self, query, params=None):
         if not self.conn:
             self.connect()
 
         try:
-            return self.conn.execute(query)
+            if params:
+                return self.conn.execute(query, params)
+            else:
+                return self.conn.execute(query)
         except Exception as e:
             print(f"Error executing query: {str(e)}")
             return None
-
-    def fetch_all(self, query):
-        result = self.execute_query(query)
-        if result:
-            return result.fetchall()
-        return None
 
     def close_connection(self):
         if self.conn:
@@ -51,59 +48,59 @@ class CommandLineApp:
 
     def get_user_info(self, user_id):
         query = 'SELECT * FROM users WHERE id = ?'
-        return self.db_connector.fetch_all(query, (user_id,))
+        return self.db_connector.execute_query(query, (user_id,))
 
     def get_all_users(self):
         query = 'SELECT * FROM users'
-        return self.db_connector.fetch_all(query)
+        return self.db_connector.execute_query(query)
 
     def get_all_orders(self):
         query = 'SELECT * FROM orders'
-        return self.db_connector.fetch_all(query)
+        return self.db_connector.execute_query(query)
 
 if __name__ == "__main__":
     db_file = 'database.db'
     app = CommandLineApp(db_file)
 
     while True:
-        print("\n1. Add User\n2. Place Order\n3. Get User Info\n4. List All Orders\n5. List all users\n6. Exit")
-        choice = input("Enter your choice: ")
+        print("\n1. Gebruiker toevoegen\n2. Order plaatsen\n3. Gebruiker info opvragen\n4. Alle orders\n5. Alle gebruikers\n6. Afsluiten")
+        choice = input("kies welke optie: ")
 
         if choice == '1':
-            name = input("Enter user name: ")
-            age = input("Enter user age: ")
+            name = input("Geef naam: ")
+            age = input("Geef leeftijd: ")
             app.add_user(name, age)
-            print("User added successfully!")
+            print("Gebruiker succesvol toegevoegd")
 
         elif choice == '2':
-            user_id = input("Enter user ID: ")
-            product = input("Enter product: ")
-            amount = input("Enter amount: ")
+            user_id = input("Geef gebruiker ID: ")
+            product = input("Geef product: ")
+            amount = input("Geef aantal: ")
             app.place_order(user_id, product, amount)
-            print("Order placed successfully!")
+            print("Order succesvol geplaatst")
 
         elif choice == '3':
-            user_id = input("Enter user ID: ")
+            user_id = input("Geef gebruiker ID: ")
             user_info = app.get_user_info(user_id)
             if user_info:
-                print("User Info:")
+                print("Gebruiker info:")
                 print(user_info)
             else:
-                print("User not found.")
+                print("Gebruiker niet gevonden.")
 
         elif choice == '4':
             all_orders = app.get_all_orders()
             if all_orders:
-                print("All Orders:")
+                print("Alle Orders:")
                 for order in all_orders:
                     print(order)
             else:
-                print("No orders found.")
+                print("Geen orders gevonden.")
 
         elif choice == '5':
             all_users = app.get_all_users()
             if all_users:
-                print("All users:")
+                print("Alle gebruikers:")
                 for user in all_users:
                     print(user)
 
@@ -111,4 +108,4 @@ if __name__ == "__main__":
             break
 
         else:
-            print("Invalid choice. Please enter a valid option.")
+            print("Ongeldige keuze.")
