a0afcd1a4a10f50c0a0685dae7e937ea0e9bd4c9
mitchelldevaere <michiel.devaere101@gmail.com>
2023-12-23 17:10:55 +0100

    aanmaken van commandline klasse. deze klasse staat in voor het versturen van de query naar de databaseconnector klasse die dan de data zal ophalen.

--- opdracht.py
+++ opdracht.py
@@ -39,3 +39,29 @@ class DatabaseConnector:
         if self.conn:
             self.conn.close()
             self.conn = None
+
+class CommandLineApp:
+    def __init__(self, db_file):
+        self.db_file = db_file
+        self.db_connector = DatabaseConnector(db_file)
+
+    def add_user(self, name, age):
+        query = 'INSERT INTO users (name, age) VALUES (?, ?)'
+        self.db_connector.execute_query(query, (name, age))
+
+    def place_order(self, user_id, product, amount):
+        query = 'INSERT INTO orders (user_id, product, amount) VALUES (?, ?, ?)'
+        self.db_connector.execute_query(query, (user_id, product, amount))
+
+    def get_user_info(self, user_id):
+        query = 'SELECT * FROM users WHERE id = ?'
+        return self.db_connector.fetch_all(query, (user_id,))
+
+    def get_all_users(self):
+        query = 'SELECT * FROM users'
+        return self.db_connector.fetch_all(query)
+
+    def get_all_orders(self):
+        query = 'SELECT * FROM orders'
+        return self.db_connector.fetch_all(query)
+
