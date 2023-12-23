1536f9ce775707a4a0914465995af1df4bc94a47
mitchelldevaere <michiel.devaere101@gmail.com>
2023-12-23 17:08:50 +0100

    veranderen van databaseconnector klasse ter voorbereiding van request te doen naar de database

--- opdracht.py
+++ opdracht.py
@@ -1,46 +1,41 @@
 import sqlite3
-import pandas as pd
 
 class DatabaseConnector:
     def __init__(self, db_file):
         self.db_file = db_file
         self.conn = None
 
+    def __enter__(self):
+        self.connect()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close_connection()
+
     def connect(self):
         self.conn = sqlite3.connect(self.db_file)
 
-    def execute_query(self, query):
+    def execute_query(self, query, params=None):
         if not self.conn:
             self.connect()
 
-        with self.conn:
-            return pd.read_sql_query(query, self.conn)
+        try:
+            with self.conn:
+                if params:
+                    return self.conn.execute(query, params)
+                else:
+                    return self.conn.execute(query)
+        except Exception as e:
+            print(f"Error executing query: {str(e)}")
+            return None
+
+    def fetch_all(self, query, params=None):
+        result = self.execute_query(query, params)
+        if result:
+            return result.fetchall()
+        return None
 
     def close_connection(self):
         if self.conn:
             self.conn.close()
             self.conn = None
-
-# Create an instance of DatabaseConnector
-db_connector = DatabaseConnector('database.db')
-
-# SQL query to retrieve the desired data
-query = '''
-    SELECT
-        users.Name AS UserName,
-        orders.Product AS Product,
-        orders.Amount AS Amount
-    FROM
-        users
-    LEFT OUTER JOIN
-        orders ON users.id = orders.user_id
-'''
-
-# Execute the query and retrieve data into a pandas DataFrame
-opdracht_data = db_connector.execute_query(query)
-
-# Close the database connection
-db_connector.close_connection()
-
-# Display the results
-print(opdracht_data)
